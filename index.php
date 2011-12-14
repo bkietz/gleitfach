4088b687d2fec2219cd835aa3c16af9fa7b88146
Benjamin Kietzman <bengilgit@gmail.com>
2011-12-13 21:11:15 -0800

    This is the initial statement of purpose for my logger system.

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+// I intend to implement a neat ~blogging system,
+// with a few specific goals:
+
+// drag-and-drop image/movie insertion
+// drag, crop, resize, etc.
+
+// the main p0st dynamic is to drag an image
+// then comment on it, with possibly more images
+
+// the main page renders (at least a header of) ~25 p0sts
+// Which 25 is determined by search terms,
+// or if none are entered, p0st youth.
+
+// p0sts can be edited afterward.
+
+// records a lot of metadata.
+
+?>
